eac0bcb8545a2cc4a49d57d6f95536f3a430aebb
ChrBank <chbl@itu.dk>
2023-03-05 19:41:22 +0100

    add kdoc to CustomArrayAdapter

--- ScooterSharing/app/src/main/java/dk/itu/moapd/scootersharing/mgan/CustomArrayAdapter.kt
+++ ScooterSharing/app/src/main/java/dk/itu/moapd/scootersharing/mgan/CustomArrayAdapter.kt
@@ -28,16 +28,33 @@ import android.view.ViewGroup
 import android.widget.ArrayAdapter
 import android.widget.TextView
 
+/**
+ * A class to customize an adapter with a `ViewHolder` to populate dataset about scooter information into a `ListView`.
+ */
 class CustomArrayAdapter(
     context: Context, private var resource: Int,
     data: List<Scooter>) :
     ArrayAdapter<Scooter>(context, R.layout.list_rides, data) {
+
+    /**
+     * An internal view holder class used to represent the layout that shows a single `Scooter`
+     * instance in the `ListView`.
+     */
     private class ViewHolder(view: View) {
         val name: TextView = view.findViewById(R.id.list_item_name)
         val location: TextView = view.findViewById(R.id.list_item_location)
         val timestamp: TextView = view.findViewById(R.id.list_item_timestamp)
     }
 
+    /**
+     * Get the `View` instance with information about a selected `Scooter` from the dataset.
+     *
+     * @param position The position of the specified item.
+     * @param convertView The current view holder.
+     * @param parent The parent view which will group the view holder.
+     *
+     * @return A new view holder populated with the selected `Scooter` data.
+     * */
     override fun getView(
         position: Int, convertView: View?,
         parent: ViewGroup
